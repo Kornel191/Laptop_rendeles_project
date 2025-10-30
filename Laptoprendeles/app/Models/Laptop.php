2c1c157fa462c9d126d076df2efe4c8c128b61d5
Kornel191 <korizsombi@gmail.com>
2025-10-30 21:00:40 +0100

    Modellek beállítása

--- Laptoprendeles/app/Models/Laptop.php
+++ Laptoprendeles/app/Models/Laptop.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Laptop extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['brand', 'model', 'price', 'stock'];
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
 }
+
--- Laptoprendeles/app/Models/Order.php
+++ Laptoprendeles/app/Models/Order.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'laptop_id', 'quantity', 'status'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function laptop()
+    {
+        return $this->belongsTo(Laptop::class);
+    }
 }
