13132e5e7c45d01e44f24b74daf89a8e1cb56c01
Kornel191 <korizsombi@gmail.com>
2025-11-02 20:23:03 +0100

    képek elérési útjának megadása

--- Laptoprendeles/database/seeders/LaptopSeeder.php
+++ Laptoprendeles/database/seeders/LaptopSeeder.php
@@ -15,71 +15,65 @@ class LaptopSeeder extends Seeder
                 'model' => 'Inspiron 15',
                 'price' => 320000,
                 'stock' => 10,
-                'image' => 'https://m.media-amazon.com/images/I/71mFv5kZ3eL._AC_SL1500_.jpg'
+                'image' => 'images/dell.png'
             ],
             [
                 'brand' => 'HP',
                 'model' => 'Pavilion 14',
                 'price' => 280000,
                 'stock' => 8,
-                'image' => 'https://m.media-amazon.com/images/I/71pV9rF+oEL._AC_SL1500_.jpg'
+                'image' => 'images/hppavilon.png'
             ],
             [
                 'brand' => 'Lenovo',
                 'model' => 'IdeaPad 3',
                 'price' => 250000,
                 'stock' => 12,
-                'image' => 'https://m.media-amazon.com/images/I/71V1IuQx8JL._AC_SL1500_.jpg'
+                'image' => 'images/lenovo.png'
             ],
             [
                 'brand' => 'Apple',
                 'model' => 'MacBook Air M2',
                 'price' => 620000,
                 'stock' => 5,
-                'image' => 'https://m.media-amazon.com/images/I/71vFKBpKakL._AC_SL1500_.jpg'
+                'image' => 'images/macbookm14.png'
             ],
             [
                 'brand' => 'Asus',
                 'model' => 'VivoBook 16',
                 'price' => 310000,
                 'stock' => 9,
-                'image' => 'https://m.media-amazon.com/images/I/71L2VZvzQVL._AC_SL1500_.jpg'
+                'image' => 'images/asus.png'
             ],
             [
                 'brand' => 'Acer',
                 'model' => 'Aspire 5',
                 'price' => 270000,
                 'stock' => 7,
-                'image' => 'https://m.media-amazon.com/images/I/71GzH3W9oRL._AC_SL1500_.jpg'
+                'image' => 'images/acer.png'
             ],
             [
                 'brand' => 'Microsoft',
                 'model' => 'Surface Laptop Go',
                 'price' => 450000,
                 'stock' => 6,
-                'image' => 'https://m.media-amazon.com/images/I/61WlWYdV5nL._AC_SL1500_.jpg'
+                'image' => 'images/microsoft.png'
             ],
             [
                 'brand' => 'Razer',
                 'model' => 'Blade 15',
                 'price' => 800000,
                 'stock' => 4,
-                'image' => 'https://m.media-amazon.com/images/I/61xc7sH8FQL._AC_SL1500_.jpg'
+                'image' => 'images/razer.png'
             ],
             [
                 'brand' => 'MSI',
                 'model' => 'GF63 Thin',
                 'price' => 550000,
                 'stock' => 3,
-                'image' => 'https://m.media-amazon.com/images/I/71O6XwW+eaL._AC_SL1500_.jpg'
+                'image' => 'images/msi.png'
             ],
-            [
-                'brand' => 'Huawei',
-                'model' => 'MateBook D15',
-                'price' => 330000,
-                'stock' => 6,
-                'image' => 'https://m.media-amazon.com/images/I/71v1AztzpsL._AC_SL1500_.jpg'
-            ],
+
         ]);
     }
 }
